2dbf71690ee4f66bab8e05f5e9adf331218f47ca
monomolekular <144608750+monomolekular@users.noreply.github.com>
2023-09-27 22:03:04 +0300

    Много чё переделал чтоб работало

--- ВтораяЧасть12Пр/Program.cs
+++ ВтораяЧасть12Пр/Program.cs
@@ -11,6 +11,8 @@ namespace ВтораяЧасть12Пр
         private static readonly int PixelsPerUnit = 25;
         private static string _defaultProgram = "1";
         private static bool _madeAction;
+        private static string _input;
+        private static string[] _cords = new [] {"0"};
 
         public Program()
         {
@@ -19,7 +21,6 @@ namespace ВтораяЧасть12Пр
 
         public static void Main(string[] args)
         {
-            //Console.Write(Console.Read());
             Console.WriteLine(
                 "Вас приветсвтует программа, разработанная Гусевым Александром Сергеевичем, обучающимся группы КМБО-01-23");
             Console.WriteLine("Для выбора дальнейших действий введите в командную строку одну из следующих цифр:");
@@ -35,6 +36,64 @@ namespace ВтораяЧасть12Пр
             else
             {
                 _defaultProgram = _defaultProgram.TrimEnd(')');
+                if (!_madeAction)
+                {
+                    switch (_defaultProgram)
+                    {
+                        case "1":
+                            Console.WriteLine("Введите координаты в формате: `x1;y1 x2;y2`");
+                            _input = Console.ReadLine();
+                            if (_input == null)
+                            {
+                                Console.WriteLine("Введена некорректная информация. Выход из программы...");
+                                Application.Exit();
+                            }
+                            else
+                            {
+                                _cords = _input.Split(' ');
+                            }
+                            break;
+                        case "2":
+                            Console.WriteLine("Введите координаты в формате: `x1;y1 x2;y2`");
+                            _input = Console.ReadLine();
+                            if (_input == null)
+                            {
+                                Console.WriteLine("Введена некорректная информация. Выход из программы...");
+                                Application.Exit();
+                            }
+                            else
+                            {
+                                _cords = _input.Split(' ');
+                            }
+                            break;
+                        case "3":
+                            Console.WriteLine("Введите координаты в формате: `x;y R`");
+                            _input = Console.ReadLine();
+                            if (_input == null)
+                            {
+                                Console.WriteLine("Введена некорректная информация. Выход из программы...");
+                                Application.Exit();
+                            }
+                            else
+                            {
+                                _cords = _input.Split(' ');
+                            }
+                            break;
+                        case "4":
+                            Console.WriteLine("Введите координаты в формате: `x;y H`");
+                            _input = Console.ReadLine();
+                            if (_input == null)
+                            {
+                                Console.WriteLine("Введена некорректная информация. Выход из программы...");
+                                Application.Exit();
+                            }
+                            else
+                            {
+                                _cords = _input.Split(' ');
+                            }
+                            break;
+                    }
+                }
                 Application.Run(new Program());
             }
         }
@@ -44,7 +103,6 @@ namespace ВтораяЧасть12Пр
             var g = e.Graphics;
             if (!_madeAction)
             {
-                DrawAxis(g);
                 switch (_defaultProgram)
                 {
                     case "1":
@@ -67,8 +125,8 @@ namespace ВтораяЧасть12Пр
         {
             for (var i = 0; i < 1000; i += PixelsPerUnit)
             {
-                g.DrawLine(Pens.LightGray, 0, i, 1000, i);
-                g.DrawLine(Pens.LightGray, i, 0, i, 1000);
+                g.DrawLine(Pens.LightGray, 0, i, 1000+PixelsPerUnit, i);
+                g.DrawLine(Pens.LightGray, i, 0, i, 1000+PixelsPerUnit);
             }
             g.DrawLine(Pens.Black, 500, 0, 500, 1000);
             g.DrawLine(Pens.Black, 0, 500, 1000, 500);
@@ -81,98 +139,54 @@ namespace ВтораяЧасть12Пр
 
         private void DrawLineByCords(Graphics g)
         {
-            Console.WriteLine("Введите координаты в формате: `x1;y1 x2;y2`");
-            var input = Console.ReadLine();
-            string[] cords = new [] {"0"};
-            if (input == null)
-            {
-                Console.WriteLine("Введена некорректная информация. Выход из программы...");
-                Application.Exit();
-            }
-            else
-            {
-                cords = input.Split(' ');
-            }
-            var x1 = 500 + int.Parse(cords[0].Split(';')[0]) * PixelsPerUnit;
-            var y1 = 500 + -1 * int.Parse(cords[0].Split(';')[1]) * PixelsPerUnit;
-            var x2 = 500 + int.Parse(cords[1].Split(';')[0]) * PixelsPerUnit;
-            var y2 = 500 + -1 * int.Parse(cords[1].Split(';')[1]) * PixelsPerUnit;
+            var x1 = 500 + int.Parse(_cords[0].Split(';')[0]) * PixelsPerUnit;
+            var y1 = 500 + -1 * int.Parse(_cords[0].Split(';')[1]) * PixelsPerUnit;
+            var x2 = 500 + int.Parse(_cords[1].Split(';')[0]) * PixelsPerUnit;
+            var y2 = 500 + -1 * int.Parse(_cords[1].Split(';')[1]) * PixelsPerUnit;
+            DrawAxis(g);
             g.DrawLine(Pens.Red, x1, y1, x2, y2);
             _madeAction = true;
         }
 
         private void DrawRectangleByCords(Graphics g)
         {
-            Console.WriteLine("Введите координаты в формате: `x1;y1 x2;y2`");
-            var input = Console.ReadLine();
-            string[] cords = new [] {"0"};
-            if (input == null)
-            {
-                Console.WriteLine("Введена некорректная информация. Выход из программы...");
-                Application.Exit();
-            }
-            else
-            {
-                cords = input.Split(' ');
-            }
-            var x1 = 500 + int.Parse(cords[0].Split(';')[0]) * PixelsPerUnit;
-            var y1 = 500 + -1 * int.Parse(cords[0].Split(';')[1]) * PixelsPerUnit;
-            var x2 = 500 + int.Parse(cords[1].Split(';')[0]) * PixelsPerUnit;
-            var y2 = 500 + -1 * int.Parse(cords[1].Split(';')[1]) * PixelsPerUnit;
+            var x1 = 500 + int.Parse(_cords[0].Split(';')[0]) * PixelsPerUnit;
+            var y1 = 500 + -1 * int.Parse(_cords[0].Split(';')[1]) * PixelsPerUnit;
+            var x2 = 500 + int.Parse(_cords[1].Split(';')[0]) * PixelsPerUnit;
+            var y2 = 500 + -1 * int.Parse(_cords[1].Split(';')[1]) * PixelsPerUnit;
             var width = Math.Abs(x2 - x1);
             var heigh = Math.Abs(y2 - y1);
             var coolCordX = x1;
             var coolCordY = y1;
             if (x1 > x2) coolCordX = x2;
             if (y1 > y2) coolCordY = y2;
+            DrawAxis(g);
             g.DrawRectangle(Pens.Red, coolCordX, coolCordY, width, heigh);
             _madeAction = true;
         }
 
         private void DrawCircleByCords(Graphics g)
         {
-            Console.WriteLine("Введите координаты в формате: `x;y R`");
-            var input = Console.ReadLine();
-            string[] cords = new [] {"0"};
-            if (input == null)
-            {
-                Console.WriteLine("Введена некорректная информация. Выход из программы...");
-                Application.Exit();
-            }
-            else
-            {
-                cords = input.Split(' ');
-            }
-            var x1 = 500 + int.Parse(cords[0].Split(';')[0]) * PixelsPerUnit;
-            var y1 = 500 + -1 * int.Parse(cords[0].Split(';')[1]) * PixelsPerUnit;
-            var r = int.Parse(cords[1].Split(';')[0]) * PixelsPerUnit;
+            var x1 = 500 + int.Parse(_cords[0].Split(';')[0]) * PixelsPerUnit;
+            var y1 = 500 + -1 * int.Parse(_cords[0].Split(';')[1]) * PixelsPerUnit;
+            var r = int.Parse(_cords[1].Split(';')[0]) * PixelsPerUnit;
             var tempCordX = x1 - r;
             var tempCordY = y1 - r;
+            DrawAxis(g);
             g.DrawEllipse(Pens.Red, tempCordX, tempCordY, 2 * r, 2 * r);
             _madeAction = true;
         }
 
         private void DrawTriangleByCords(Graphics g)
         {
-            Console.WriteLine("Введите координаты в формате: `x;y H`");
-            var input = Console.ReadLine();
-            string[] cords = new [] {"0"};
-            if (input == null)
-            {
-                Console.WriteLine("Введена некорректная информация. Выход из программы...");
-                Application.Exit();
-            }
-            else
-            {
-                cords = input.Split(' ');
-            }
-            var x1 = 500 + int.Parse(cords[0].Split(';')[0]) * PixelsPerUnit;
-            var y1 = 500 + -1 * int.Parse(cords[0].Split(';')[1]) * PixelsPerUnit;
-            var h = int.Parse(cords[1]) * PixelsPerUnit;
+            var x1 = 500 + int.Parse(_cords[0].Split(';')[0]) * PixelsPerUnit;
+            var y1 = 500 + -1 * int.Parse(_cords[0].Split(';')[1]) * PixelsPerUnit;
+            var h = int.Parse(_cords[1]) * PixelsPerUnit;
             var side = (float)Math.Sqrt(4 * Math.Pow(h, 2) / 3);
             var x2 = side / 2 + x1;
             var y2 = y1 - h;
             var p = Pens.Red;
+            DrawAxis(g);
             g.DrawLine(p, x1, y1, x2, y2);
             g.DrawLine(p, x1 + side, y1, x2, y2);
             g.DrawLine(p, x1, y1, x1 + side, y1);
@@ -194,10 +208,11 @@ namespace ВтораяЧасть12Пр
             resources.ApplyResources(this, "$this");
             AutoValidate = AutoValidate.Disable;
             BackColor = SystemColors.Window;
-            ClientSize = new Size(784, 561);
-            SizeGripStyle = SizeGripStyle.Hide;
-            StartPosition = FormStartPosition.CenterScreen;
+            ClientSize = new Size(1000, 1000);
+            //SizeGripStyle = SizeGripStyle.Hide;
+            //StartPosition = FormStartPosition.CenterScreen;
             CausesValidation = false;
+            Text = "График выбранной программы";
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Program";
